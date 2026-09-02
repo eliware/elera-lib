2859548af25d0e94e6b91d70d211eb86de2cf3be
Eli Sterling <eli@eliware.org>
2026-09-01 20:52:41 -0400

    Rename credential validator modules

--- src/bundle/fields/index.mjs
+++ src/bundle/fields/index.mjs
@@ -1,5 +1,5 @@
 import { validateBundleCoreFields } from './core.mjs';
-import { validateBundleCredentials } from './credentials.mjs';
+import { validateBundleCredentials } from './auth.mjs';
 import { validateBundleScopes } from './scopes.mjs';
 
 export function validateBundleFields(bundle) {
